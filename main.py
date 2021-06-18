a08b7bd4098dfdf04cf14e76deb047a51593b77b
Yukwok <billynyk@gmail.com>
2021-06-18 11:10:37 +0800

    twillo added, sms services

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import requests
+from twilio.rest import Client
 
+# open weather map details
 api_key = "9f6959f38e1944621c0d10d01720caa5"
-city = "Shanghai"
+city = "suzhou"
 parameters = {
     "appid": api_key,
-    "q": city
+    "lat": 31.298973,
+    "lon": 120.585289,
+    "exclude": "current,minutely,daily"
 }
-weather_url = "https://api.openweathermap.org/data/2.5/weather"
+# twillo details
+account_sid = "ACfc1f66926937dff5407161913da6f046"
+auth_token = "4f8e654118b63c378612a438f110034c"
+
+weather_url = "https://api.openweathermap.org/data/2.5/onecall"
 
 response = requests.get(url=weather_url, params=parameters)
 data = response.json()
-print(data["weather"])
+sliced_data = data['hourly'][:12]
 
+will_rain = False
+
+for hour_data in sliced_data:
+    weather_id = hour_data['weather'][0]['id']
+    if weather_id < 800:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    print("will rain")
+    message = client.messages \
+        .create(
+        body="Bring the urmbella",
+        from_="+18303411303",
+        to="+85290986832"
+    )
+    print(message.status)
