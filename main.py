6da208b81c00ae1f73b1c1a2fecd500c4d29f084
Yukwok <billynyk@gmail.com>
2021-06-18 08:29:49 +0800

    test api call weather app

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import requests
+
 api_key = "9f6959f38e1944621c0d10d01720caa5"
+city = "Shanghai"
+parameters = {
+    "appid": api_key,
+    "q": city
+}
+weather_url = "api.openweathermap.org/data/2.5/weather"
 
-api.openweathermap.org/data/2.5/weather?q=Shanghai&appid=9f6959f38e1944621c0d10d01720caa5
-
-# sdgdgdsfgdfsg
-
-
+response = requests.get(url=weather_url, params=parameters)
+print(response)
