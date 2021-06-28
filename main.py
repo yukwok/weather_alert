00b52c43903da7b2ff8bf43391a90a6ea1d5a67d
Yukwok <billynyk@gmail.com>
2021-06-28 09:54:22 +0800

    final work version

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
 
+from twilio.http.http_client import TwilioHttpClient
+
 import requests
 from twilio.rest import Client
+import pprint
 
 # # open weather map details  ---- use for cloud server
 # api_key = os.environ.get("OWM_APIKEY")
@@ -31,36 +34,37 @@ parameters = {
     "exclude": "current,minutely,daily"
 }
 # twillo details
+# proxy_client = TwilioHttpClient(
+#     proxy={'http': os.environ['http_proxy'], 'https': os.environ['https_proxy']})
+
 account_sid = "ACfc1f66926937dff5407161913da6f046"
-auth_token = "4f8e654118b63c378612a438f110034c"
+auth_token = "8de9dae43f75ed764b3040487b2f0726"
+
+client = Client(account_sid, auth_token)
 
 weather_url = "https://api.openweathermap.org/data/2.5/onecall"
 
 response = requests.get(url=weather_url, params=parameters)
 data = response.json()
-sliced_data = data['hourly'][:12]
+sliced_data = data['hourly'][:3]
+
+pprint.pprint(sliced_data)
 
 will_rain = False
 
 for hour_data in sliced_data:
     weather_id = hour_data['weather'][0]['id']
-    if weather_id < 800:
+    if weather_id < 600 and weather_id > 499:
         will_rain = True
 
+
+print(will_rain)
 if will_rain:
-    client = Client(account_sid, auth_token)
     print("will rain")
     message = client.messages \
         .create(
-        body="Bring the urmbella",
-        from_="+18303411303",
-        to="+85290986832"
-    )
+            body="Rain within 3 hrs, bring the urmbella",
+            from_="+18303411303",
+            to="+85290986832"
+        )
     print(message.status)
-
-    #
-    # os.environ.get()
-    # #
-    # # TWILLO_TOKEN = 4f8e654118b63c378612a438f110034c
-    # #
-    # # OWM_APIKEY = 9f6959f38e1944621c0d10d01720caa5
